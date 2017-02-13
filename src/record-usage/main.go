4fe0d9e5b15dd6c4c4596221a30442f3a1fff3f7
Wei Mao <weimao1@gmail.com>
2017-02-13 15:29:45 -0800

    EnumChildWindow callback

--- src/record-usage/main.go
+++ src/record-usage/main.go
@@ -12,6 +12,21 @@ func main() {
 		return
 	}
 
-	processID := GetWindowThreadProcessId(hwnd)
-	fmt.Println(processID)
+	processID := GetWindowProcessId(hwnd)
+	fmt.Println("pid: ", processID)
+
+	processHandle := OpenProcess(processID)
+	if processHandle == 0 {
+		return
+	}
+
+	windowText := GetWindowText(hwnd)
+	fmt.Println(windowText)
+
+	if IsImmersiveProcess(processHandle) {
+		CloseHandle(processHandle)
+		hwnd, processID = GetUniversalApp(hwnd, processID)
+		processHandle = OpenProcess(processID)
+		fmt.Println("pid: ", processID)
+	}
 }
--- src/record-usage/winapi.go
+++ src/record-usage/winapi.go
@@ -2,6 +2,7 @@ package main
 
 import "unsafe"
 import "golang.org/x/sys/windows"
+import "syscall"
 
 type (
 	BOOL      int32
@@ -9,6 +10,14 @@ type (
 	HANDLE    uintptr
 	HINSTANCE HANDLE
 	HWND      HANDLE
+	WPARAM    uint64
+	LPARAM    int64
+	WCHAR     uint16
+)
+
+const (
+	FALSE = 0
+	TRUE  = 1
 )
 
 var (
@@ -21,8 +30,67 @@ func GetForegroundWindow() HWND {
 	return HWND(hwnd)
 }
 
-func GetWindowThreadProcessId(hwnd HWND) DWORD {
+func GetWindowProcessId(hwnd HWND) DWORD {
 	var processID DWORD
 	user32.NewProc("GetWindowThreadProcessId").Call(uintptr(hwnd), uintptr(unsafe.Pointer(&processID)))
 	return DWORD(processID)
+}
+
+func OpenProcess(processID DWORD) HANDLE {
+	const PROCESS_QUERY_INFORMATION = 0x00000400
+	const PROCESS_VM_READ = 0x0010
+
+	handle, _, _ := kernel32.NewProc("OpenProcess").Call(
+		PROCESS_QUERY_INFORMATION|PROCESS_VM_READ,
+		FALSE,
+		uintptr(processID))
+	return HANDLE(handle)
+}
+
+func CloseHandle(handle HANDLE) {
+	kernel32.NewProc("CloseHandle").Call(uintptr(handle))
+}
+
+func GetWindowText(hwnd HWND) string {
+	var buffer [256]uint16
+	user32.NewProc("GetWindowTextW").Call(uintptr(hwnd), uintptr(unsafe.Pointer(&buffer)), 256)
+	return windows.UTF16ToString(buffer[:])
+}
+
+func IsImmersiveProcess(handle HANDLE) bool {
+	result, _, _ := user32.NewProc("IsImmersiveProcess").Call(uintptr(handle))
+	return result != FALSE
+}
+
+func GetUniversalApp(hwnd HWND, processID DWORD) (HWND, DWORD) {
+
+	type enumParameter struct {
+		hwnd           HWND
+		processID      DWORD
+		childHwnd      HWND
+		childProcessID DWORD
+	}
+	parameter := enumParameter{hwnd: hwnd, processID: processID}
+
+	callback := syscall.NewCallback(
+		// Use uintptr instead of BOOL, because: syscall.NewCallback requires
+		// the callback's returns size = unsafe.Sizeof(uintptr(0))
+		// ref: \go\src\runtime\syscall_windows.go, syscall.compileCallback()
+		func(childHwnd HWND, lparam LPARAM) uintptr {
+			parameter := (*enumParameter)(unsafe.Pointer(uintptr(lparam)))
+			childProcess := GetWindowProcessId(childHwnd)
+			if childProcess != 0 && childProcess != parameter.processID {
+				parameter.childHwnd = childHwnd
+				parameter.childProcessID = childProcess
+				return FALSE
+			}
+			return TRUE
+		})
+
+	user32.NewProc("EnumChildWindows").Call(
+		uintptr(hwnd),
+		callback,
+		uintptr(unsafe.Pointer(&parameter)))
+
+	return parameter.childHwnd, parameter.childProcessID
 }
